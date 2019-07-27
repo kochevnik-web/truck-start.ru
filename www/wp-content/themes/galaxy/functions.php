3d09c1e524179a2d4056021c442b6dca0227fbcc
Dmitriy Kovalev Home <kodimweb@gmail.com>
2019-07-28 01:43:24 +0300

    added header

--- www/wp-content/themes/galaxy/functions.php
+++ www/wp-content/themes/galaxy/functions.php
@@ -1,5 +1,137 @@
 <?php
 
+class True_Walker_Nav_Menu extends Walker_Nav_Menu{
+
+	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+		if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+			$t = '';
+			$n = '';
+		} else {
+			$t = "\t";
+			$n = "\n";
+		}
+		$indent = str_repeat( $t, $depth );
+
+		// Default class.
+		$classes = array( 'sub-menu', 'dropdown-menu' );
+
+		$class_names = join( ' ', apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth ) );
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+		$output .= "{$n}{$indent}<ul$class_names>{$n}";
+	}
+
+	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+			$t = '';
+			$n = '';
+		} else {
+			$t = "\t";
+			$n = "\n";
+		}
+		$indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
+
+		$classes   = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+		if ( in_array( 'menu-item-has-children', $classes) ) {
+			$classes[] = 'dropdown';
+		}
+
+		$args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
+
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args, $depth );
+		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+		$output .= $indent . '<li' . $id . $class_names . '>';
+
+		$atts           = array();
+		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+		$atts['target'] = ! empty( $item->target ) ? $item->target : '';
+		if ( '_blank' === $item->target && empty( $item->xfn ) ) {
+			$atts['rel'] = 'noopener noreferrer';
+		} else {
+			$atts['rel'] = $item->xfn;
+		}
+		$atts['href']         = ! empty( $item->url ) ? $item->url : '';
+		$atts['aria-current'] = $item->current ? 'page' : '';
+
+		$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+
+		$attributes = '';
+		foreach ( $atts as $attr => $value ) {
+			if ( ! empty( $value ) ) {
+				$value       = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+				$attributes .= ' ' . $attr . '="' . $value . '"';
+			}
+		}
+
+		/** This filter is documented in wp-includes/post-template.php */
+		$title = apply_filters( 'the_title', $item->title, $item->ID );
+
+		$title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
+
+		$caret = $args->walker->has_children == 1 ? '<span class="ecaret"></span>' : '';
+		$item_output  = $args->before;
+		$item_output .= '<a' . $attributes . '>';
+		$item_output .= $args->link_before . $title . $args->link_after . $caret;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+
+}
+
+/**
+ * Регистрируем меню
+ */
+
+register_nav_menus(array(
+	'main'   => 'Главное меню',
+));
+
+/**
+ * кастомная функция вывода данных
+ */
+
+if ( ! function_exists( 'dump' ) ) {
+	function dump( $var ) {
+		echo '<pre style="color: #c3c3c3; background-color: #282923;">';
+		print_r( $var );
+		echo '</pre>';
+	}
+}
+
+/**
+ * Выводим страницу настроек шаблона через ACF
+ */
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Основные настройки',
+		'menu_title'	=> 'Настройки темы',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Настройки шапки',
+		'menu_title'	=> 'Шапка',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Настройки подвала',
+		'menu_title'	=> 'Подвал',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+	
+}
+
 	/**
 	 * Подключение файлов CSS и JS шаблона
 	 */
@@ -7,18 +139,18 @@
 	add_action( 'wp_enqueue_scripts', 'galaxy_enqueue' );
 	function galaxy_enqueue() {
 
-		if ( MINIFY ) {
-			wp_enqueue_style( 'block', get_template_directory_uri() . '/css/block.css' );
-			wp_enqueue_style( 'galaxy', get_template_directory_uri() . '/css/style.min.css' );
-
-			wp_enqueue_script( 'galaxy', get_template_directory_uri() . '/js/scripts.min.js', array( 'jquery' ), '1.0.0', true );
-		} else {
-			wp_enqueue_style( 'block', get_template_directory_uri() . '/css/block.css' );
-			wp_enqueue_style( 'galaxy', get_template_directory_uri() . '/css/style.css' );
-			
-			wp_enqueue_script( 'galaxy', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), '1.0.0', true );
-
-		}
+		wp_enqueue_style( 'changetemplatecolormain', get_template_directory_uri() . '/css/change_color_template_main.css' );
+		wp_enqueue_style( 'js_composer_min', get_template_directory_uri() . '/css/js_composer.min.css' );
+		wp_enqueue_style( 'bootstrap_min', get_template_directory_uri() . '/css/bootstrap.min.css' );
+		wp_enqueue_style( 'bootstrap-submenu', get_template_directory_uri() . '/css/bootstrap-submenu.css' );
+		wp_enqueue_style( 'animate-min', get_template_directory_uri() . '/css/animate.min.css' );
+		wp_enqueue_style( 'slick', get_template_directory_uri() . '/css/slick.css' );
+		wp_enqueue_style( 'iconfont', get_template_directory_uri() . '/css/iconfont.css' );
+		wp_enqueue_style( 'magnific-popup', get_template_directory_uri() . '/css/magnific-popup.css' );
+		wp_enqueue_style( 'scrolling-tabs', get_template_directory_uri() . '/css/scrolling-tabs.css' );
+		wp_enqueue_style( 'galaxy', get_template_directory_uri() . '/css/style.css' );
+		
+		wp_enqueue_script( 'galaxy', get_template_directory_uri() . '/js/scripts.min.js', array( 'jquery' ), '1.0.0', true );
 
 	}
 
@@ -30,14 +162,17 @@ add_action( 'after_setup_theme', 'galaxy_theme_setup' );
 function galaxy_theme_setup() {
 	
 	/** Add theme support for core framework features. */
-	add_theme_support( 'galaxy-core-menus', array( 'galaxy-primary-menu' ) );
-	add_theme_support( 'galaxy-core-sidebars', array( 'galaxy-primary-sidebar' ) );
-	add_theme_support( 'galaxy-core-featured-image' );
-	add_theme_support( 'galaxy-core-custom-header' );
+	add_theme_support('menus');
+	add_theme_support( 'custom-logo', [
+		'height'      => 75,
+		'width'       => 210,
+		'flex-width'  => false,
+		'flex-height' => false,
+		'header-text' => '',
+	] );
 	
 	/** Add theme support for WordPress features. */
 	add_theme_support( 'automatic-feed-links' );
-	
 
 }
 
--- www/wp-content/themes/galaxy/header.php
+++ www/wp-content/themes/galaxy/header.php
@@ -4,232 +4,145 @@
 		<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 		<title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="<?php echo get_bloginfo( 'template_url' ) ?>/img/favicon.inc"/>
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&display=swap&subset=cyrillic" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<?php wp_head(); ?>
-
-		<style id='car-repair-services-style-inline-css' type='text/css'>
-			body{
-				font-family: 'Open Sans', sans-serif;
-				font-weight:400;
-				font-size:16px;
-				line-height:22px;
-			}
-
-			header.page-header-1 .header-right .address::after {
-				border-color: transparent rgba(19,19,19,1) transparent transparent;
-			}
-		
-			@media ( min-width: 992px ) {
-		
-				/*--------   2.2 Header          --------*/
-				body.home header.page-header {
-					position: absolute;
-					background: none;
-				}
-
-				body:not(.home) {
-					background: url(https://smartdata.tonytemplates.com/car-repair-service/car2/wp-content/uploads/sites/6/2018/10/header-photo-bg.jpg) no-repeat center 0;
-				}
-
-			}
-
-			@media ( max-width: 991px ) {
-				#pageTitle {
-					background: url(https://smartdata.tonytemplates.com/car-repair-service/car2/wp-content/uploads/sites/6/2018/10/header-photo-bg.jpg) repeat 0;
-					background-size: cover;
-				}
-			}
-
-			body:not(.home) #pageContent {
-				padding-top: 100px;
-			}
-
-			h1{
-				font-weight:600;
-				font-size:40px;
-				line-height:40px;
-				color:#000;
-			}
-
-			h2{
-				font-weight:600;
-				font-size:34px;
-				line-height:40px;
-				color:#000;
-			}
-			h3{
-				font-size:30px;
-				line-height:36px;
-				color:#000;
-			}
-			h4{
-				font-size:36px;
-				line-height:32px;
-				color:#000;
-			}
-			h5{
-				font-weight:700;
-				font-size:20px;
-				line-height:28px;
-				color:#000;
-			}
-			h6{
-				font-size:16px;
-				line-height:24px;
-				color:#000;
-			}
-
-			.widget-title, .title-contact-info, .widgettitle{
-				font-size:24px;
-				color:#000;
-			}
-
-			#mainSlider .slide-content h4{
-				font-weight:200;
-			}
-
-			#mainSlider .slide-content h3{
-				font-weight:400;
-			}
-
-			.filters-row .page-numbers .current{
-				background-color:transparent !important;
-				color:#fede00;
-			}
-
-			header.page-header-2 .header-topline [class*='icon'] {
-				color:#fede00;
-			}
-
-			body.layout-2 h1:after, body.layout-2 h2.h-lg:after {
-				background-color:#fede00;
-			}
-
-			@media ( min-width: 992px ) {
-				header.page-header-2 .navbar-nav > li.current-menu-item > a:after, header.page-header-2 .navbar-nav > li:hover > a:after {
-					background-color:#fede00;
-				}
-			}
-
-			header.page-header-2 .search-container:hover .button {
-				background-color:#fede00;
-			}
-
-			#mainSlider .banner-btn, #mainSlider .banner-btn:focus{
-				border: 2px solid #fede00;       
-			}
-
-			.services-tabs .services-tabs-icons > span.active {
-				background-color:#fede00;
-			}
-
-			.services-tabs .services-tab-info:after {
-				color:#fede00;
-			}
-
-			.icons-tabs .nav-tabs > li.active > a [class*='icon-'], .icons-tabs .nav-tabs > li > a:hover [class*='icon-'] {
-				color:#fede00;
-			}
-
-			.icons-tabs .nav-tabs > li > a::after {
-				background:#fede00;
-			}
-
-			.marker-list-sm-1 > li:after {
-				color:#fede00;
-			}
-
-			.how-works-number {
-				color:#fede00;
-			}
-
-			.banner-free .banner-text-1 { 
-				background-color:#fede00;
-			}
-
-			.banner-free .banner-text-1:after {
-				border-color: #fede00 transparent transparent transparent;
-			}
-
-			.pricing-box-header {
-				background-color:#fede00;
-			}
-
-			.text-icon-hor2 .icon-wrapper2 {
-				color:#fede00;
-			}
-
-			#appointmentForm.modalform-sm .wpcf7-submit.btn {
-				border-color:#fede00;
-			}
-
-			body .textarea-custom:hover, 
-			body .textarea-custom:focus, 
-			body .input-custom:hover, 
-			body .input-custom.focus, 
-			body .wpcf7-form-control.wpcf7-textarea:hover, 
-			body .wpcf7-form-control.wpcf7-textarea:focus {
-				border-color: #fede00!important;
-			}
-
-			.bootstrap-datetimepicker-widget table td.active, 
-			.bootstrap-datetimepicker-widget table td.active:hover {
-				background-color: #fede00!important;
-			}
-
-			div.wpcf7-validation-errors, div.wpcf7-acceptance-missing {
-				border-color:#fede00;
-			}
-
-			@media ( min-width: 992px ) {
-				header.page-header-2 .navbar-nav .dropdown-menu {
-					border-bottom-color:#fede00;
-				}
-			}
-
-			.pricing-box-footer .mark-icon {
-				color:#fede00;
-			}
-
-			@media (max-width: 991px){
-				header.page-header-2 .navbar-toggle:hover {
-					color:#fede00;
-				}
-			}
-
-			.filters-row .page-numbers .current {
-				color:#fede00;
-			}
-
-			.estimator-panel .col-title [class*='icon'] {
-				color:#fede00;
-			}
-
-			.service-grid-item:hover .service-grid-item-title {
-				background-color:#fede00;
-			}
-
-			.service-grid-item-title:after {
-				border-color: transparent transparent #fede00 transparent;
-			}
-
-			@media (max-width: 767px){
-				.estimator-panel .panel-toggle {
-					color:#fede00;
-				}
-			}
-
-			.car-faq-text .vc_toggle_title:hover h4 {
-				color:#fede00;
-			}
-
-			.btn-invert:before, .btn-invert:after {
-				background-color: #2c2c2c !important;
-			}
-		</style>
+		<script type='text/javascript'>
+		/* <![CDATA[ */
+		var changetemplatecolor_object = {"changetemplatecolor_ajax_url":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/color-setting.png","changetemplate_logo_red":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/logo-red-2.png","changetemplate_logo_orange":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/logo-orange-2.png","changetemplate_logo_yellow":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/logo-2.png","changetemplate_logo_green":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/logo-green-2.png","changetemplate_logo_turquoise":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/logo-turquoise-2.png","changetemplate_logo_blue":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/logo-blue-2.png","changetemplate_logo_violet":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/logo-violet-2.png","color_url":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/css\/"};
+		var changetemplateimg_object = {"ajax_url":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-admin\/admin-ajax.php","setting_icon":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/layout-setting.png","layout1_img":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/layout-1.png","layout2_img":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/layout-2.png","layout3_img":"https:\/\/smartdata.tonytemplates.com\/car-repair-service\/car2\/wp-content\/plugins\/Color-Change-template\/img\/layout-3.png"};
+		/* ]]> */
+		</script>
 		
 	</head>
-	<body class="home page-template-default page page-id-37 logged-in admin-bar no-customize-support">
+		<body <?php body_class(); ?>>
+		    <!-- Loader -->
+			<div id="loader-wrapper">
+				<div class="loader">
+					<div class="line"></div>
+					<div class="line"></div>
+					<div class="line"></div>
+					<div class="line"></div>
+					<div class="line"></div>
+					<div class="line"></div>
+					<div class="subline"></div>
+					<div class="subline"></div>
+					<div class="subline"></div>
+					<div class="subline"></div>
+					<div class="subline"></div>
+					<div class="loader-circle-1">
+						<div class="loader-circle-2"></div>
+					</div>
+					<div class="needle"></div>
+					<div class="loading"><?php _e( 'Загрузка', 'galaxy' ); ?></div>
+				</div>
+			</div>
+			<!-- //Loader -->
+			<!-- Header -->
+			<header class="page-header-2 sticky">
+            	<div class="header-info-mobile">
+        			<div class="header-info-mobile-inside">
+						<p>
+							<i class="icon icon-locate"></i>
+							<?php echo get_field( 'header_adres', 'option' ); ?>
+						</p>
+						<p>
+							<i class="icon icon-phone"></i>
+							<?php echo get_field( 'header_phone', 'option' ); ?>
+						</p>
+						<p>
+							<i class="icon icon-email"></i>
+							<?php echo get_field( 'header_email', 'option' ); ?>
+						</p>
+						<?php if ( get_field( 'header_clock', 'option' ) != '' ) { ?>
+						<p>
+							<i class="icon icon-clock"></i>
+							<?php echo get_field( 'header_clock', 'option' ); ?>
+						</p>
+						<?php } ?>
+					</div>
+				</div>
+
+				<div class="header-topline">
+                    <div class="header-info-toggle">
+						<i class="icon-locate js-info-toggle"></i>
+					</div>
+            		<div class="header-right-top">
+                        <a href="#" class="appointment" data-toggle="modal" data-target="#appointmentForm"><i class="icon-shape icon">
+						</i>
+							<span>Онлайн заявка</span>
+						</a>
+					</div>
+					
+					<div class="container">
+                		<div class="row-flex">
+                    		<div class="col-left"><i class="icon icon-clock"></i>
+								Время работы: <?php echo get_field( 'header_clock', 'option' ); ?>
+							</div>
+
+							<div class="col-center">
+								<i class="icon icon-phone"></i>
+								<span class="hidden-md">
+									Кпуглосуточно:
+								</span>
+								<a href="tel:<?php echo get_field( 'header_hot_phone', 'option' ); ?>" class="header-phone"><?php echo get_field( 'header_hot_phone', 'option' ); ?></a>
+							</div>
+							
+							<div class="col-right">
+                        		<i class="icon icon-locate"></i>
+								<?php echo get_field( 'header_adres', 'option' ); ?>
+							</div>
+                		</div>
+            		</div>
+				</div>
+				
+				<nav class="navbar" id="slide-nav">
+            		<div class="container">
+                		<div class="header-row">
+                            <div class="logo">
+								<?php echo get_custom_logo(); ?>
+							</div>
+
+							<?php
+								wp_nav_menu( array(
+									'container'       => 'div',
+									'container_class' => '',
+									'container_id'    => 'slidemenu',
+									'menu_class'      => 'nav navbar-nav',
+									'menu_id'         => 'menu-primary-menu',
+									'before'          => '',
+									'after'           => '',
+									'link_before'     => '',
+									'link_after'      => '',
+									'depth'           => 0,
+									'walker'          => new True_Walker_Nav_Menu(),
+									'theme_location'  => 'main'
+								) );
+							?>
+
+							<div class="header-row-right">
+                                <div class="search-container-wrap">
+                            		<div class="search-container">
+                                		<form role="search" method="get" class="search-form" action="">
+											<input type="search" id="search-form-5d3a22aaacad2" class="search-field" 
+    placeholder="Поиск &hellip;" value="" name="s" />
+											<button type="submit" class="button">
+												<i class="icon icon-search"></i>
+											</button>
+										</form>
+									</div>
+								</div>
+								
+								<button type="button" class="navbar-toggle">
+									<i class="icon icon-lines-menu"></i>
+								</button>
+                    	</div>
+                	</div>
+            	</div>
+        	</nav>
+    	</header>
+							
+
 
